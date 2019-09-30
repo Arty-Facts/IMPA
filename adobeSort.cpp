bbdb96d8a9bc5b48a8d065808ccd4d55540f6009
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-09-30 11:43:34 +0200

    sync

---
+++ adobeSort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+  int main(void) {
+    std::string row;
+  
+    while(std::cin >> row) {
+        std::cout << row << std::endl;
+    }
+
+    return 0;
+  }
---
+++ autori.py
@@ -0,0 +1,6 @@
+#python3 
+import sys
+ans = ""
+for i in sys.stdin.readline().split("-"):
+    ans += i[0]
+print(ans)
